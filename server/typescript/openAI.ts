bc0f90f90ac83be37eb7020da2dd4640840295b8
danwahlin <dwahlin@xmlforasp.net>
2023-07-01 14:41:49 -0700

    Updates to BYOD/AI features & general refactoring

--- server/typescript/openAI.ts
+++ server/typescript/openAI.ts
@@ -1,24 +1,32 @@
 import fs from 'fs';
 import { OpenAIApi, Configuration } from 'openai';
-import { QueryData, AzureOpenAIResponse, EmailSmsResponse } from './interfaces';
+import { QueryData, AzureOpenAIResponse, EmailSmsResponse, OpenAIHeadersBody, ChatGPTData } from './interfaces';
 import fetch from 'cross-fetch';
 import './config';
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const OPENAI_ENDPOINT = process.env.OPENAI_ENDPOINT;
-const OPENAI_MODEL = process.env.OPENAI_MODEL;
-const OPENAI_API_VERSION = process.env.OPENAI_API_VERSION;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY as string;
+const OPENAI_ENDPOINT = process.env.OPENAI_ENDPOINT as string;
+const OPENAI_MODEL = process.env.OPENAI_MODEL as string;
+const OPENAI_API_VERSION = process.env.OPENAI_API_VERSION as string;
+const AZURE_COGNITIVE_SEARCH_ENDPOINT = process.env.AZURE_COGNITIVE_SEARCH_ENDPOINT as string;
+const AZURE_COGNITIVE_SEARCH_KEY = process.env.AZURE_COGNITIVE_SEARCH_KEY as string;
+const AZURE_COGNITIVE_SEARCH_INDEX = process.env.AZURE_COGNITIVE_SEARCH_INDEX as string;
 
-async function getAzureOpenAICompletion(systemPrompt: string, userPrompt: string, temperature = 0): Promise<string> {
+async function getAzureOpenAICompletion(systemPrompt: string, userPrompt: string, temperature: number): Promise<string> {
 
-    if (!OPENAI_API_KEY || !OPENAI_ENDPOINT || !OPENAI_MODEL) {
-        throw new Error('Missing Azure OpenAI API key, endpoint, or model in environment variables.');
+    const requiredEnvVars = ['OPENAI_API_KEY', 'OPENAI_ENDPOINT', 'OPENAI_MODEL'];
+
+    for (const envVar of requiredEnvVars) {
+        if (!process.env[envVar]) {
+            throw new Error(`Missing ${envVar} in environment variables.`);
+        }
     }
 
     // While it's possible to use the OpenAI SDK (as of today) with a little work, we'll use the REST API directly for Azure OpenAI calls.
-    // https://learn.microsoft.com/en-us/azure/cognitive-services/openai/reference
-    const url = `${OPENAI_ENDPOINT}/openai/deployments/${OPENAI_MODEL}/chat/completions?api-version=${OPENAI_API_VERSION}`;
-    const data = {
+    // https://learn.microsoft.com/azure/cognitive-services/openai/reference
+    const chatGptUrl = `${OPENAI_ENDPOINT}/openai/deployments/${OPENAI_MODEL}/chat/completions?api-version=${OPENAI_API_VERSION}`;
+
+    const messageData: ChatGPTData = {
         max_tokens: 1024,
         temperature,
         messages: [
@@ -27,26 +35,106 @@ async function getAzureOpenAICompletion(systemPrompt: string, userPrompt: string
         ]
     };
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'api-key': `${OPENAI_API_KEY}`,
-            },
-            body: JSON.stringify(data),
-        });
+    const headersBody: OpenAIHeadersBody = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': OPENAI_API_KEY
+        },
+        body: JSON.stringify(messageData),
+    };
 
-        const completion = await response.json() as AzureOpenAIResponse;
+    try {
+        const response = await fetch(chatGptUrl, headersBody);
+        const completion: AzureOpenAIResponse = await response.json();
+        console.log(completion);
+
         let content = (completion.choices[0]?.message?.content?.trim() ?? '') as string;
         console.log('Azure OpenAI Output: \n', content);
+
         if (content && content.includes('{') && content.includes('}')) {
             content = extractJson(content);
         }
+
         return content;
     }
     catch (e) {
         console.error('Error getting data:', e);
+        throw e;
+    }
+}
+
+async function getAzureOpenAIBYODCompletion(systemPrompt: string, userPrompt: string, temperature: number): Promise<string> {
+
+    const requiredEnvVars = [
+        'OPENAI_API_KEY',
+        'OPENAI_ENDPOINT',
+        'OPENAI_MODEL',
+        'AZURE_COGNITIVE_SEARCH_ENDPOINT',
+        'AZURE_COGNITIVE_SEARCH_KEY',
+        'AZURE_COGNITIVE_SEARCH_INDEX',
+    ];
+
+    for (const envVar of requiredEnvVars) {
+        if (!process.env[envVar]) {
+            throw new Error(`Missing ${envVar} in environment variables.`);
+        }
+    }
+
+    // https://learn.microsoft.com/en-us/azure/cognitive-services/openai/use-your-data-quickstart?tabs=command-line&pivots=rest-api
+    const byodUrl = `${OPENAI_ENDPOINT}/openai/deployments/${OPENAI_MODEL}/extensions/chat/completions?api-version=${OPENAI_API_VERSION}`;
+
+    const messageData: ChatGPTData = {
+        max_tokens: 1024,
+        temperature,
+        messages: [
+            { role: 'system', content: systemPrompt },
+            { role: 'user', content: userPrompt }
+        ],
+        // Adding BYOD data source so that Cognitive Search is used with Azure OpenAI
+        dataSources: [
+            {
+                type: 'AzureCognitiveSearch',
+                parameters: {
+                    endpoint: AZURE_COGNITIVE_SEARCH_ENDPOINT,
+                    key: AZURE_COGNITIVE_SEARCH_KEY,
+                    indexName: AZURE_COGNITIVE_SEARCH_INDEX
+                }
+            }
+        ]
+    };
+
+    const headersBody: OpenAIHeadersBody = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': OPENAI_API_KEY,
+            chatgpt_url: byodUrl.replace('extensions/', ''),
+            chatgpt_key: OPENAI_API_KEY
+        },
+        body: JSON.stringify(messageData),
+    };
+
+    try {
+        const response = await fetch(byodUrl, headersBody);
+        const completion = await response.json();
+        console.log(completion);
+
+        if (completion.error) {
+            return completion.error.message;
+        }
+
+        const citations = completion.choices[0].messages[0].content.trim();
+        console.log('Azure OpenAI BYOD Citations: \n', citations);
+
+        let content = completion.choices[0].messages[1].content.trim();
+        console.log('Azure OpenAI BYOD Output: \n', content);
+
+        return content;
+
+    }
+    catch (e) {
+        console.error('Error getting BYOD data:', e);
         throw e;
     }
 }
@@ -71,7 +159,7 @@ async function getOpenAICompletion(systemPrompt: string, userPrompt: string, tem
             ]
         });
 
-        let content = extractJson(completion.data.choices[0]?.message?.content?.trim() ?? '') as string;
+        let content = extractJson(completion.data.choices[0]?.message?.content?.trim() ?? '');
         console.log('OpenAI Output: \n', content);
         if (content && content.includes('{') && content.includes('}')) {
             content = extractJson(content);
@@ -84,15 +172,21 @@ async function getOpenAICompletion(systemPrompt: string, userPrompt: string, tem
     }
 }
 
-function callOpenAI(systemPrompt: string, userPrompt: string, temperature = 0) {
+function callOpenAI(systemPrompt: string, userPrompt: string, temperature = 0, useBYOD = false) {
     const isAzureOpenAI = OPENAI_API_KEY && OPENAI_ENDPOINT && OPENAI_MODEL;
+
+    if (isAzureOpenAI && useBYOD) {
+        // Call endpoint that combines Azure OpenAI with Cognitive Search for custom data sources.
+        return getAzureOpenAIBYODCompletion(systemPrompt, userPrompt, temperature);
+    }
+
     if (isAzureOpenAI) {
         return getAzureOpenAICompletion(systemPrompt, userPrompt, temperature);
     }
-    else {
-        return getOpenAICompletion(systemPrompt, userPrompt, temperature);
-    }
+
+    return getOpenAICompletion(systemPrompt, userPrompt, temperature);
 }
+
 
 function extractJson(content: string) {
     const regex = /\{(?:[^{}]|{[^{}]*})*\}/g;
@@ -105,33 +199,41 @@ function extractJson(content: string) {
     }
 }
 
-async function getSQL(userPrompt: string): Promise<QueryData> {
+async function completeBYOD(userPrompt: string): Promise<string> {
+    const systemPrompt = 'You are an AI assistant that helps people find information.';
+    // Pass that we're using Cognitive Search along with Azure OpenAI.
+    return await callOpenAI(systemPrompt, userPrompt, 0, true);
+}
+
+async function getSQLFromNLP(userPrompt: string): Promise<QueryData> {
     // Get the high-level database schema summary to be used in the prompt.
     // The db.schema file could be generated by a background process or the 
     // schema could be dynamically retrieved.
     const dbSchema = await fs.promises.readFile('db.schema', 'utf8');
 
     const systemPrompt = `
-    Assistant is a natural language to SQL bot that returns only a JSON object with the SQL query and 
-    the parameter values in it. The SQL will query a PostgreSQL database.
-    
-    PostgreSQL tables, with their columns:    
-
-    ${dbSchema}
-
-    Rules:
-    - Convert any strings to a PostgreSQL parameterized query value to avoid SQL injection attacks.
-    - Always return a JSON object with the SQL query and the parameter values in it.
-    - Only return a JSON object. Do NOT include any text outside of the JSON object. Do not provide any additional explanations or context. 
-      Just the JSON object is needed.
-    - Example JSON object to return: { "sql": "", "paramValues": [] }
+      Assistant is a natural language to SQL bot that returns only a JSON object with the SQL query and 
+      the parameter values in it. The SQL will query a PostgreSQL database.
+      
+      PostgreSQL tables, with their columns:    
+  
+      ${dbSchema}
+  
+      Rules:
+      - Convert any strings to a PostgreSQL parameterized query value to avoid SQL injection attacks.
+      - Always return a JSON object with the SQL query and the parameter values in it.
+      - Only return a JSON object. Do NOT include any text outside of the JSON object. Do not provide any additional explanations or context. 
+        Just the JSON object is needed.
+      - Example JSON object to return: { "sql": "", "paramValues": [] }
     `;
 
     let queryData: QueryData = { sql: '', paramValues: [], error: '' };
     let results = '';
+
     try {
         results = await callOpenAI(systemPrompt, userPrompt);
         if (results) {
+            console.log('results', results);
             const parsedResults = JSON.parse(results);
             queryData = { ...queryData, ...parsedResults };
             if (isProhibitedQuery(queryData.sql)) {
@@ -139,16 +241,12 @@ async function getSQL(userPrompt: string): Promise<QueryData> {
                 queryData.error = 'Prohibited query.';
             }
         }
-    } 
-    catch (e) {
-        console.log(e);
-        // Completion results may still contain SQL information we don't want to expose.
-        // so check to ensure it's OK to return it to client.
+    } catch (error) {
+        console.log(error);
         if (isProhibitedQuery(results)) {
             queryData.sql = '';
             queryData.error = 'Prohibited query.';
-        }
-        else {
+        } else {
             queryData.error = results;
         }
     }
@@ -220,4 +318,4 @@ async function completeEmailSMSMessages(prompt: string, company: string, contact
     return content;
 }
 
-export { completeEmailSMSMessages, getSQL };
+export { completeBYOD, completeEmailSMSMessages, getSQLFromNLP as getSQL };
